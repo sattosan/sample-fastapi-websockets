7e5ddadece32be1047015f64f6a1c99c00adb82e
sattosan <sato.yuya1211@gmail.com>
2020-02-24 04:17:24 +0900

    Add コメントの追加

--- app/app.js
+++ app/app.js
@@ -7,11 +7,13 @@ const server = http.createServer((req,res)=>{
   // ファイル読み込み
   fs.readFile('index.html','UTF-8',
     (error, data) => {
+        // エラー処理
         if (error) {
             response.writeHead(500, {"Content-Type": "text/plain"});
             response.write("500 Internal Server Error\n");
             res.end();
         }
+        // index.htmlを表示
         res.writeHead(200,{'Content-Type':'text/html'});
         res.write(data);
         res.end();
