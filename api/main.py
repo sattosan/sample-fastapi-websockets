dfb149b2273ac673e33e0e00cc6b5db9564a6db4
satto_sann <sato.yuya1211@gmail.com>
2020-05-03 00:38:34 +0900

    Update async/await対応とプッシュ通知ロジックを切り出し

--- api/main.py
+++ api/main.py
@@ -1,26 +1,39 @@
 from fastapi import FastAPI
-from starlette.websockets import WebSocket
+from starlette.websockets import WebSocket, WebSocketDisconnect
+
+from notifier import Notifier
+
 
 app = FastAPI()
+# プッシュ通知各種設定が定義されているインスタンス
+notifier = Notifier()
 
-# 接続中のクライアントを識別するためのヘッダを格納
-clients = {}
-
-# WebSockets用のエンドポイント
+# Websocket用のパス
 @app.websocket("/ws")
-async def websocket_endpoint(ws: WebSocket):
-    await ws.accept()
-    # クライアントを識別するためのヘッダを取得
-    key = ws.headers.get('sec-websocket-key')
-    clients[key] = ws
+async def websocket_endpoint(websocket: WebSocket):
+    # クライアントとのコネクション確立
+    await notifier.connect(websocket)
     try:
         while True:
-            # クライアントからメッセージを受信
-            data = await ws.receive_text()
-            # 接続中のクライアントそれぞれにメッセージを送信（ブロードキャスト）
-            for client in clients.values():
-                await client.send_text(f"ID: {key} | Message: {data}")
-    except:
-        await ws.close()
-        # 接続が切れた場合、当該クライアントを削除する
-        del clients[key]
+            # クライアントからメッセージの受け取り
+            data = await websocket.receive_text()
+            # 双方向通信する場合
+            #  await websocket.send_text(f"Message text was: {data}")
+            # ブロードキャスト
+            await notifier.push(f"Message text was: {data}")
+    # セッションが切れた場合
+    except WebSocketDisconnect:
+        # 切れたセッションの削除
+        notifier.remove(websocket)
+
+# ブロードキャスト用のAPI
+@app.get("/push/{message}")
+async def push_to_connected_websockets(message: str):
+    # ブロードキャスト
+    await notifier.push(f"! Push notification: {message} !")
+
+# サーバ起動時の処理
+@app.on_event("startup")
+async def startup():
+    # プッシュ通知の準備
+    await notifier.generator.asend(None)
